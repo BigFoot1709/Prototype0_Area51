c3e19fab8842206009196ea832055ca5070a56b9
Daingab <47778568+Daingab@users.noreply.github.com>
2019-07-19 08:32:04 +0200

    Small change - shoot script

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    private float bulletSpeed = 10f;
+    public float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 1f)
+        {
+            Destroy(gameObject);
+        }
+
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(0f, bulletSpeed * Time.deltaTime, 0f);
+        pos += transform.rotation * velocity;
+        transform.position = pos;
+    }
+}
