1ffbccbbc132695312a728d7a9641cb971003b64
Daingab <47778568+Daingab@users.noreply.github.com>
2019-07-21 14:01:02 +0200

    AI implemented.

---
+++ Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject bulletSpawn;
+    public GameObject bulletPrefab2;
+    public GameObject bulletSpawn2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int enemyLayer = 1 << 9;
+        enemyLayer = ~enemyLayer;
+
+        RaycastHit hit;
+
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, 3f, enemyLayer))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                GetComponent<Patrol>().enabled = false;
+                Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+            GetComponent<Patrol>().enabled = true;
+        }
+    }
+}
--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -5,7 +5,54 @@ using UnityEngine;
 public class Patrol : MonoBehaviour
 {
     public float speed;
+    public float distance;
     private bool movingRight = true;
     public Transform groundDetection;
+    public Collider colliderMe;
+    public Collider colliderU;
 
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        //Physics.IgnoreCollision(colliderMe, colliderU);
+
+        /*RaycastHit groundInfo = Physics.Raycast(groundDetection.position, Vector3.down, distance);
+
+        if (groundInfo.collider == true)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0f, -180f, 0f);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+                movingRight = true;
+            }
+        }*/
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Door")
+        {
+            print("door!");
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0f, -180f, 0f);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+                movingRight = true;
+            }
+        }
+    }
 }
